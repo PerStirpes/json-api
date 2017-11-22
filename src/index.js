b74e3122da0a10a88c37fdfc968fb7be33f6838e
Matt Barraza-Carl <PerStirpes@users.noreply.github.com>
2017-11-21 22:35:05 -0800

    small tweaks

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ export default async () => {
   const url = `https://api.github.com/emojis`;
   const response = await fetch(url);
   const assets = await response.json();
-
-  if (response.status !== 200) throw Error(assets.message);
+  console.log(response.status);
+  if (!response.ok) throw Error(assets.message);
 
   return assets;
 };
