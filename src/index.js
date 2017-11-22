f85fa48016a05e4bc9a1687b341c5232c0982fa3
Matt Barraza-Carl <PerStirpes@users.noreply.github.com>
2017-11-21 21:42:28 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import fetch from "node-fetch";
 
 export default async () => {
-  const url = `https://api.github.com`;
+  const url = `https://api.github.com/emojis`;
   const response = await fetch(url);
-  const asset = await response.json();
+  const assets = await response.json();
 
-  if (response.status !== 200) throw Error(asset.message);
+  if (response.status !== 200) throw Error(assets.message);
 
-  return asset.dragon;
+  return assets;
 };
