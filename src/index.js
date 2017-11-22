29f7676a9dc0366b0fe770e8374583ca97cf6d12
Matt Barraza-Carl <PerStirpes@users.noreply.github.com>
2017-11-21 22:37:34 -0800

    prettier

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import fetch from "node-fetch";
+import fetch from 'node-fetch'
 
 export default async () => {
-  const url = `https://api.github.com/emojis`;
-  const response = await fetch(url);
-  const assets = await response.json();
-  console.log(response.status);
-  if (!response.ok) throw Error(assets.message);
+  const url = `https://api.github.com/emojis`
+  const response = await fetch(url)
+  const assets = await response.json()
+  console.log(response.status)
+  if (!response.ok) throw Error(assets.message)
 
-  return assets;
-};
+  return assets
+}
